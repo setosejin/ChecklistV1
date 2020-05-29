3a91cbc7e64331bd839be4f478187f5cac32da1a
Sejin Kim <lemon05090@gmail.com>
2020-05-29 23:33:39 +0900

    Initial Commit

---
+++ ChecklistV1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChecklistV1
+//
+//  Created by 김세진 on 2020/05/29.
+//  Copyright © 2020 김세진. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
