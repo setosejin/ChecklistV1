91a80fd04fd8d8eec98a7b7085ce4bf1f4c97ce7
Sejin Kim <lemon05090@gmail.com>
2020-06-05 01:46:50 +0900

    Change the starting scene

---
+++ ChecklistV1/EditChecklistitemView.swift
@@ -0,0 +1,21 @@
+//
+//  EditChecklistitemView.swift
+//  ChecklistV1
+//
+//  Created by 김세진 on 2020/06/05.
+//  Copyright © 2020 김세진. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditChecklistitemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditChecklistitemView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditChecklistitemView()
+    }
+}
